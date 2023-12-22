e4900832f682427af8096a04b1b23d2875bd50e4
nasimrifat101 <nasimrifat101@gmail.com>
2023-12-22 16:43:29 +0600

    data aos added

--- src/Pages/components/Home/Home.jsx
+++ src/Pages/components/Home/Home.jsx
@@ -1,19 +1,40 @@
+import Aos from "aos";
+import "aos/dist/aos.css";
 import { Banner, Footer, Navbar, NewsLetter, Review, WhoUse, Whyus } from "./components";
-
-
-
+import { useEffect } from "react";
 
 const Home = () => {
+    useEffect(() => {
+        Aos.init();
+    }, []);
+
     return (
         <div>
-            <Navbar/>
-            <Banner/>
-            <Whyus/>
-            <WhoUse/>
-            <Review/>
-            <NewsLetter/>
-            <Footer/>
-            
+
+            <Navbar />
+            <div data-aos="fade-up" data-aos-duration="1000">
+
+                <Banner />
+            </div>
+            <div data-aos="fade-up" data-aos-duration="1000">
+
+                <Whyus />
+            </div>
+            <div data-aos="fade-up" data-aos-duration="1000">
+
+                <WhoUse />
+            </div>
+            <div data-aos="fade-up" data-aos-duration="1000">
+
+                <Review />
+            </div>
+            <div data-aos="fade-up" data-aos-duration="1000">
+
+                <NewsLetter />
+            </div>
+
+            <Footer />
+
         </div>
     );
 };
